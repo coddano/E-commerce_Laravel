e76882cf26f6e474dc99cbfd500b07d380cf6879
coddano <totondejourdain@gmail.com>
2022-10-08 02:23:38 +0100

    debut

---
+++ database/seeders/ProductsTablesSeedder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory;
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProductsTablesSeedder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+
+        for ($i=0 ; $i < 30 ; $i++ ) { 
+            Product::create([
+                'title' => $faker->sentence(15),
+                'slug' =>$faker->slug,
+                'subtitle' => $faker->sentence(10),
+                'description' => $faker->text,
+                'price' => $faker->numberBetween(15, 300) + 100,
+                'image' => 'https://via.placeholder.com/200x250'
+
+            ]);
+        }
+    }
+}
